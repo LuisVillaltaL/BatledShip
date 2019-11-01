fd0f946a7058142c853a572dab8e99a44bdab22b
LuisVillaltaL <50061503+LuisVillaltaL@users.noreply.github.com>
2019-11-01 00:46:57 -0600

    Update Game BattledShip.cpp
    se agregaron funciones para realizar ataque a naves 
    se agrego mensaje para ganador

--- Game BattledShip.cpp
+++ Game BattledShip.cpp
@@ -1,164 +1,231 @@
+/*	=======================================================================
+--	Juego BattledShip Algoritmos <Author,LV>
+--  Fecha de creacion <Create DAte,,> '2019-10-06'
+--  Fecha de Modificacion <Create DAte,,> '2019-10-30', Agregar Ataque
+	=======================================================================
+*/
 #include <iostream>
-#include<stdio.h>
-#include<stdlib.h>
-#include<windows.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
 
 using namespace std;
-
-void PrintBarco();
-void jugar();
-
-//	================== 
-// 	Variables globales
+//	==================
+//	Variables Globales 
 typedef int vida;
-int jugador1[20][20], jugador2[20][20], puntajejugador1 = 0, puntajejugador2 = 0, i, j, k, opcion1, opcion2, rojo, azul,menu, salir;
+int op, x, y, i;
+char **a=NULL,**b=NULL,**ta=NULL,**tb=NULL;
+vida jugador2 = 5, jugador1 = 5;
+//	=======================
+// 	LLamada a las Funciones
+char** inicio();
+void mover(char **p);
+void liberar(char **p);
+void tablero(char **p);
+void turno(int i);
+void msj_ganador();
+vida cont_vidas(char **p);
+void ingresar(char **p,int i);
+void atacar(char **p,char **pos,vida barcos,vida enemigo);
+void juego(char **p,char **enemigo,char **posienemigo,vida vbarcos,vida venemigo);
+void jugar(char **a,char **b,char **ta,char **tb,vida jugador2,vida jugador1);
+void PrintBarco();
 
+//	=======================================
+//	Menu Principal llama a la funcion Jugar 
+int main(){
+	PrintBarco();
+	cin >> op;
+	 b = inicio();
+	 a = inicio();
+	ta = inicio();
+	tb = inicio();
 
-//	==========================================
-//	Inicia programa Menu y llamada a funciones
-int main()
-{
-	do{
-		PrintBarco();
-		cin >>menu;
-		
-		switch(menu)
-		{
-			case 1: 
-				jugar(); 
-			break;
-			case 2:
-				system ("cls");
-				cout << "Confirme que desea salir " << endl;
-				cout << "1. Si" << endl;
-				cout << "2. No, Regresa a Menu? " <<endl;
-				cin >>salir;
-				if(salir == 1){
-					return 0;
-				}else{
-					return main();
-				}
-				
-				system ("pause");
-				system ("cls");
-			break;
-		}
-	}while(menu !=3);
+	system("cls");
+	switch(op){
+		case 1:
+			jugar (a,b,ta,tb,jugador2, jugador1);
+		break;
+	}
 	
-	return 0;
-}
+	liberar(a);
+	liberar(b);
+	liberar(ta);
+	liberar(tb);
 
-//	===========================================
-//	Funcion imprime mensaje de turno de jugador
-void mensaje_turno(int i){
+}
+//	=========================
+// Funcion para dos jugadores
+void jugar(char **a,char **b,char **ta,char **tb,vida jugador2,vida jugador1){
+	turno(1);
+	tablero(a);
+	for(i = 0; i <= 4; i++){
+		ingresar(a,i);
+		tablero(a);
+	}
+	
+	turno(2);
+	tablero(b);
+	for(i = 0; i <= 4; i++){
+		ingresar(b,i);
+		tablero(b);
+	}
+	system("cls");
+	
+	for(i = 1; jugador1 > 0 && jugador2 > 0;i++){
+		turno(i);
+		
+		if(i%2==0)juego(a,tb,b,jugador1,jugador2);
+		else juego(b,ta,a,jugador2,jugador1);
+		
+		jugador1 = cont_vidas(a);
+		jugador2 = cont_vidas(b);	
+	}
+	
+	getchar();
+	system("cls");
+}
+//	==================================
+//	Funcion Imprimir Tablero del Juego
+void tablero(char **p){
+	int i,j;
+	for(i = 0; i <= 20; i++)p[0][i];
+	for(i = 0; i <= 20;i++)p[i][0];
+	for(i = 0; i <= 20;i++){
+		printf("\n\t\t\t\t");
+		for(j = 0; j <= 20;j++)
+			printf(" %c",p[i][j]);
+	}
+}
+char** inicio(){
+	char **p=NULL;
+	int i,j;
+	
+	p=(char**)malloc(20*sizeof(char*));
+	for(i = 0; i <= 20; i++)
+		p[i]=(char*)malloc(20*sizeof(char));
+		
+	for(j = 0; j <= 20; j++)
+		for(i = 0; i <= 20;i++)
+			p[j][i]='_';	
+			
+	return p;
+}
+void liberar(char **p){
+	int i;
+	for(i=0;i<=20;i++)free(p[i]);
+	free(p);
+}
+//	=================================================
+//	Funcion para Imprimir Mensaje de Turno de Jugador
+void turno(int i){
 	system("cls");
 	printf("\n\n\n\n\n\n\n\n\n\t\t\t\t");
 	Sleep(60*10);
 	if(i%2==0){
-		
-		printf("Turno Jugador 2");
+		printf("Turno de Jugador 2");
 		Sleep(60*30);
-		system("color b");
+		
 	}
 	else{
-
-		printf("Turno Jugador 1");
+		printf("Turno del Jugador 1");
 		Sleep(60*30);
-		system("color c");
+		
 	}
-	for(i=1;i<5;i++)putchar('\n');
+	for(i=0;i<20;i++)putchar('\n');
 	
 	system("cls");	
 }
-void imprimir(char **p){
+
+vida cont_vidas(char **p){
 	int i,j;
-	for(i=0;i<10;i++)p[0][i]='0'+i;
-	for(i=0;i<10;i++)p[i][0]='0'+i;
-	for(i=0;i<10;i++){
-		printf("\n\t\t\t\t");
-		for(j=0;j<10;j++)
-			printf(" %c",p[i][j]);
-	}
+	vida cont = 0;
+	for(i = 0; i < 20; i++)
+		for(j = 0; j < 20;j++){
+			if(p[i][j]>='A' && p[i][j]<='K')
+			cont++;
+		}
+	return cont;
 }
-//	=======================================
-// 	Funcion que imprime el tablero de juego
-void tablero() {   
-    // tablero del jugador
-    for(i = 1; i <= 20;i++) // Imprime el tablero del jugador
-    {
-        printf("\t");
-        for(j = 1; j <= 20;j++)
-        {
-            if(jugador1[i][j]==3)
-            {
-                printf("  #");
-            }
-            else
-            {
-                if(jugador1[i][j]==2) // Imprime 'X' si vale 2 || 'O' si vale 1
-                {
-                    printf("  X");
-                }
-                else
-                {
-                    if(jugador1[i][j]==1)
-                    {
-                        printf("  O");
-                    }
-                    else
-                    {
-                        printf("  -");
-                    }
-                }
-            }                                    
-        }
- 
-    cout << "\n";  
-}
-
-}
-// ================================================
-// funcion llenado de Tablero
-void iniciar(){ 
+//	=======================================================
+//	Funcion Generar tablero para realizar ataque a oponente
+void juego(char **p,char **enemigo,char **posienemigo,vida vbarcos,vida venemigo){
+	int op;
 	system("cls");
-    for( k = 1; k <= 5; k++) // Distribuye los barcos
-    {
-        imprimir(char **p);
-
-        jugador1[i][j]=1; // La posicion de los barcos valdra 1
-                       
-		cout << "\n\n Ingrese sus coordenadas\n\n";
-        printf("\n\tX%d = ",k);
-        scanf("%d", &opcion2);
-                         
-        printf("\n\tY%d = ", k);
-        scanf("%d", &opcion1);
-       
-        jugador1[opcion1][opcion2]=1;
-                  	    
-    system("cls");
-                                                         
-    }  
-     
-}
-
-// Funcion turno de jugadores
-void jugar(){
-	system ("cls");
-	mensaje_turno(1);
-	for(i = 0; i <= 4; i++){
-		iniciar();
-
-	}
+	cout << "\n\t Tienes "<<vbarcos << " Municiones ";
+	tablero(p);
+	printf("\n\t\t\t 1. Atacar\n\t\t\t ");
+	printf("\n\t\t\t 2. Turno del Oponente\n\t\t\t ");
+	scanf("%d",&op);
 	
-	mensaje_turno(2);
-	for(i = 0; i <= 4; i++){
-		iniciar();
-		
+	switch(op){
+		case 1:atacar(enemigo,posienemigo,vbarcos,venemigo);break;
+		case 2: exit;
 	}
-	system("pause");
+	fflush(stdin);
+}
+
+//	Funcion Atacar posiciones de Barcos
+void atacar(char **p,char **pos,vida barcos,vida enemigo){
+	int i,x=0,y=0;
+	for(i=0;i<barcos && enemigo>0;i++){
+		system("cls");
+		system("color 0B");
+		printf("\t\t\t Ingresa la posicion que desea Atacar\n");
+		tablero(p);
+		x=0;
+		do{
+			if(x!=0)printf("\n\t\t Posicion ya Fue Atacada");
+			
+			printf("\n\t\t\tx: ");
+			scanf("%d",&x);
+			printf("\t\t\tY: ");
+			scanf("%d",&y);
+			
+		}while(pos[y][x]=='#' || pos[y][x]=='-');
+		
+		if(pos[y][x]>='A' && pos[y][x]<='K'){
+			enemigo--;
+			pos[y][x]=p[y][x]='#';
+			printf("\a");
+		}
+		else pos[y][x]=p[y][x]='-';
+	}
+	if(enemigo==0)msj_ganador();
+}
+
+//	=============================================
+// 	Funciona Ingresa las coordenadas de las Naves
+void ingresar(char **p,int i){
+	int x=0,y=0;
+	printf("\n\t\t\t Ingresa las cordenadas de tu barco %d\n",i+1);
+	do{
+		if(x!=0)printf("\n\t\t\t\tYA EXISTE ESTE BARCO!!\n");
+		printf("\t\t\tx: ");
+		scanf("%d",&x);
+		printf("\t\t\ty: ");
+		scanf("%d",&y);
+		
+	}while(p[y][x]>='A' && p[y][x]<='K');
+	
 	system("cls");
+	p[y][x]='A'+i;
+}
+void msj_ganador(){
+	int i;
+	system("cls");
+	system("color fd");
+	printf("\n\n\n\n\n\n\n\n\t\t    ");
+	Sleep(60*10);
+	char cad[]={"Has Destruido la Flota Enemiga"};
+	for(i=0;i<cad[i]!='\0';i++){
+		putchar(cad[i]);
+		Sleep(30);
+	}
+	Sleep(60*60);
+	printf("\n\n\t\t    <ENTER>");
+	getchar();
 }
 
 //	=====================================
